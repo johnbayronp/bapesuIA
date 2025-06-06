1b2e95f8e14c28745a21f83c9adc83fe8489a5e5
John Bayron P <webdeprueba1.1@gmail.com>
2025-06-05 23:38:15 -0500

    cors active full - maybe

--- server/app/app.py
+++ server/app/app.py
@@ -4,7 +4,16 @@ from app.config import Config
 
 app = Flask(__name__)
 
-app.config.from_object(Config)
+CORS(app, 
+         resources={r".*": {  # Permitir todas las rutas
+             "origins": Config.CORS_ORIGINS,
+             "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+             "allow_headers": ["Content-Type", "Authorization", "Accept"],
+             "expose_headers": ["Content-Type", "Authorization"],
+             "supports_credentials": True
+         }},
+         supports_credentials=True)
 
+app.config.from_object(Config)
 
 from app import routes
