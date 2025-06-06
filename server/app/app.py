55c224bd7ed38a911c5404ab932f7912fab90faf
John Bayron P <webdeprueba1.1@gmail.com>
2025-06-05 23:47:56 -0500

    test

--- server/app/app.py
+++ server/app/app.py
@@ -3,16 +3,8 @@ from flask_cors import CORS
 from app.config import Config
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
-CORS(app, 
-         resources={r".*": {  # Permitir todas las rutas
-             "origins": Config.CORS_ORIGINS,
-             "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-             "allow_headers": ["Content-Type", "Authorization", "Accept"],
-             "expose_headers": ["Content-Type", "Authorization"],
-             "supports_credentials": True
-         }},
-         supports_credentials=True)
 
 app.config.from_object(Config)
 
